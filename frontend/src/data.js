c37bcdaa506914087fbf5dedd31b69f9709281d6
dionelo <dionelmartin@gmail.com>
2020-11-12 10:11:56 -0300

    create react app

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Naike Slim Shirt',
+            category: 'Shirts',
+            image: 'images/p1.jpg',
+            price: 120,
+            brand: 'Naike',
+            rating: 4,
+            numReviews: 15,
+            description: 'High quality camisuli'
+        },
+        {
+            _id: '2',
+            name: 'Addidas Slim Shirt',
+            category: 'Shirts',
+            image: 'images/p2.jpg',
+            price: 80,
+            brand: 'Addidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality camisuli'
+        },
+        {
+            _id: '3',
+            name: 'Lacosta Slim Shirt',
+            category: 'Shirts',
+            image: 'images/p3.jpg',
+            price: 120,
+            brand: 'Lacosta',
+            rating: 2.5,
+            numReviews: 10,
+            description: 'High quality camisuli'
+        },
+        {
+            _id: '4',
+            name: 'Naike Super Pants',
+            category: 'Pants',
+            image: 'images/p4.jpg',
+            price: 100,
+            brand: 'Naike',
+            rating: 3.5,
+            numReviews: 8,
+            description: 'High quality lompas'
+        },
+        {
+            _id: '5',
+            name: 'Puna Slim Pants',
+            category: 'Pants',
+            image: 'images/p5.jpg',
+            price: 130,
+            brand: 'Puna',
+            rating: 5,
+            numReviews: 18,
+            description: 'High quality pantaloni'
+        },
+        {
+            _id: '6',
+            name: 'Addidas Fits Pants',
+            category: 'Pants',
+            image: 'images/p6.jpg',
+            price: 125,
+            brand: 'Addidas',
+            rating: 3,
+            numReviews: 7,
+            description: 'High quality lompas'
+        },
+    ]
+}
+
+export default data;
